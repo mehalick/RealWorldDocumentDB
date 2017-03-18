684e895e850c58b0c11ae7b1d501712a37a013f9
Andy Mehalick <andy.mehalick@orangejetpack.com>
2017-03-18 22:38:36 +0300

    added entities project

---
+++ src/RealWorldDocumentDB.Entities/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RealWorldDocumentDB.Entities
+{
+    public abstract class EntityBase
+    {
+        [JsonProperty(PropertyName = "id", Order = -4)]
+        public string Id { get; set; }
+
+        [JsonProperty(Order = -3)]
+        public string Type => GetType().ToString();
+
+        [JsonProperty(Order = -2)]
+        public DateTime CreatedUtc { get; set; }
+    }
+
+    public class Movie : EntityBase
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Metascore { get; set; }
+    }
+
+    public class Photo : EntityBase
+    {
+        public string Caption { get; set; }
+        public Uri ImageUri { get; set; }
+        public double FileSize { get; set; }
+    }
+}
---
+++ src/RealWorldDocumentDB.Entities/Photo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RealWorldDocumentDB.Entities
+{
+    public class Photo : EntityBase
+    {
+        public string Caption { get; set; }
+        public Uri ImageUri { get; set; }
+        public double FileSize { get; set; }
+    }
+}
